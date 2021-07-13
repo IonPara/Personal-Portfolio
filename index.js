b57d29d1a6fbc73152f06a415cab6b06388d867b
IonPara <84988051+IonPara@users.noreply.github.com>
2021-07-13 19:03:31 +0100

    Add the code for about-me and skills containers

---
+++ index.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("myNavpanel").style.width = "200px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("myNavpanel").style.width = "0";
+  }
